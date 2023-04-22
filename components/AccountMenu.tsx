ec654cd345a30ec68a437f5da9dc81ef722dcd09
Ayrtonalvaro <ayrtonalvaro1973@gmail.com>
2023-04-22 10:00:34 -0300

    Add favorite list, movie watch

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { signOut } from 'next-auth/react'
+import useCurrentUser from '@/hooks/useCurrentUser'
 
 interface AccountMenuProps{
   visible?: boolean
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ( {visible} ) => {
-
+  const { data: currentUser } = useCurrentUser()
   if(!visible) {
     return null
   }
@@ -17,12 +18,12 @@ const AccountMenu: React.FC<AccountMenuProps> = ( {visible} ) => {
         <div className='px-3 group/item flex flex-row gap-3 items-center w-full'>
           <img className='w-8 rounded-md' src='/images/default-red.png' alt='imgProfile'/>
           <p className='text-white text-sm group-hover/item:underline'>
-            Username
+            {currentUser?.name}
           </p>
           <hr className='bg-gray-600 border-0 h-px my-4'/>
           
         </div>
-        <div onAbort={() => signOut} className='px-3 text-center text-white text-sm hover:underline'>
+        <div onAbort={() => signOut()} className='px-3 text-center text-white text-sm hover:underline'>
             Sign out of Netflix
           </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Billboard from "@/components/Billboard"
 import MovieList from "@/components/MovieList"
 import Navbar from "@/components/Navbar"
 import useCurrentUser from "@/hooks/useCurrentUser"
+import useFavorites from "@/hooks/useFavorites"
 import useMovieList from "@/hooks/useMovieList"
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
@@ -29,13 +30,15 @@ export async function getServerSideProps (context: NextPageContext) {
 export default function Home() {
   
   const { data: movies = []} = useMovieList()
+  const { data:favorites = []} = useFavorites()
 
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
-      <MovieList title="TRending now" data={movies}/>
+      <MovieList title="Trending now" data={movies}/>
+      <MovieList title="My list" data={favorites}/>
       </div>
     </>
     
